071d998beb03c57ac78a52f450ab4e22517d5719
诺夏 <nuoxia.lp@taobao.com>
2014-07-06 01:08:18 +0800

    update 更改目录结构

--- NXSegmentedControlDemo/NXViewController.m
+++ NXSegmentedControlDemo/NXViewController.m
@@ -21,7 +21,7 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-    NSArray *segments = [NSArray arrayWithObjects:@"1", @"2", nil];
+    NSArray *segments = [NSArray arrayWithObjects:@"店铺动态", @"达人导购", nil];
     UISegmentedControl *sc = [[UISegmentedControl alloc]initWithItems:segments];
     sc.frame = CGRectMake(20.f, 20.f, 280.f, 32.f);
     sc.tintColor = RGB(0x5f, 0x64, 0x6e);
@@ -34,6 +34,18 @@
     }];
     nxsc.frame = CGRectMake(20.f, 56.f, 280.f, 32.f);
     nxsc.selectedSegmentIndex = 0;
+    nxsc.selectedSegmentColor = [UIColor blueColor];
+    nxsc.unselectedSegmentColor = [UIColor brownColor];
+    nxsc.selectedTextColor = [UIColor whiteColor];
+    nxsc.unselectedTextColor = [UIColor whiteColor];
+    nxsc.borderColor = [UIColor blackColor];
+    nxsc.cornerRadius = 0.f;
+    nxsc.borderWidth = 8.f;
+    
+    [nxsc insertSegmentTitle:@"3" atIndex:2];
+    
+    nxsc.fontSize = 12.f;
+    
     [self.view addSubview:nxsc];
 }
 
